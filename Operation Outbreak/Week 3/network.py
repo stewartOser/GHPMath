d4740a1b0ffea3ab2651e3fa602a0518b2d6dfa5
stewartOser <stewart.oser@gmail.com>
2025-06-30 11:25:50 -0400

    network files

---
+++ Operation Outbreak/Week 3/network.py
@@ -0,0 +1,20 @@
+from simulation_network import run_simulation, Network
+from SIR_network import model
+# Note lowercase on "model"; we're importing the actual model instance, not the class
+
+nw = Network(model)
+nw.add_nodes(5, "Susceptible")   # Add 5 nodes with initial state "Susceptible"
+nw.set_state(0, "Infected")      # Initial state for node 0
+nw.add_edges(                    # Define edges between nodes
+    (0, 1),
+    (1, 2),
+    (1, 3),
+    (3, 4)
+)
+
+# Show how to print information about the underlying graph
+g = nw.get_nx_graph()
+print(g.nodes)
+
+run_simulation(model, nw, num_frames=10)
+
